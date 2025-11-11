e1c1022e73949b428e5436fc9987b0fe2249c022
Jen <87286211+jenlith@users.noreply.github.com>
2025-11-11 10:49:49 -0500

    Create team_setup.sql
    basic start

---
+++ team_setup.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS app_user (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(200) NOT NULL
+);
+
+INSERT INTO app_user VALUES
+('admin', 'scrypt:32768:8:1$PhncmQzv0oxH1KfF$e603eba9106c933b1d516ba61b73c94ff82e31b0cf1dcfedb33e506e4481bfeb45644d7523e361409ca119e98c89d5b1b8cb1191fae2f285ee59c3bf2890e095');
